c25dcc54a41df0ae8d2029d2523d0df385ac0e06
Gavdzynskyi Viacheslav <62297406+GavdzinskyiVeacheslav@users.noreply.github.com>
2022-01-29 15:20:24 +0200

    Normally hid the secret key in the virtual environment

--- coolsite/coolsite/settings.py
+++ coolsite/coolsite/settings.py
@@ -1,10 +1,12 @@
-
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = '---'
+SECRET_KEY = os.getenv('SECRET_KEY')
+print(SECRET_KEY)
 
 DEBUG = True
 
